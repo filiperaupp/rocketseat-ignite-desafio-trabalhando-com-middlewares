775b06ac3f06b86118247ebcb65ba921a5cc6e8b
Filipe Raupp <filipe.raupp@hotmail.com>
2022-02-07 19:51:12 -0300

    feat(middlewares): criando middlewares do desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,51 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) response.status(404);
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ((!user.pro && user.todos.length < 10) || user.pro)
+    next();
+  
+  return response.status(403);
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  if (!user) return response.status(404);
+
+  const isValidId = validate(id);
+  if (!isValidId) return response.status(400);
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo)  return response.status(404).json({ error: "To do not found!" });
+
+  request.user = user;
+  request.todo = todo;
+  next(); 
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  if (user) {
+    request.user = user;
+    next();
+  }
+
+  return response.status(404);
 }
 
 app.post('/users', (request, response) => {
